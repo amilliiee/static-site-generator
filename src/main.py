846fc099a5f1ac7166d17c3d3b88f9d097287b91
amilliiee <alsylvester@hotmail.com>
2025-04-15 23:36:16 +0000

    live site

--- src/main.py
+++ src/main.py
@@ -1,17 +1,27 @@
 import os
 import shutil
+import sys
 
-from textnode import TextNode, TextType
 from copystatic import recursive_file_copy, generate_pages_recursive
 
 def main():
-    static_dir = os.path.join(os.getcwd(), "static")
-    public_dir = os.path.join(os.getcwd(), "public")
-
+    static_dir = "./static"
+    public_dir = "./docs"
+    content_dir = "./content"
+    template_path = "./template.html"
+    
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
     if os.path.exists(public_dir):
         shutil.rmtree(public_dir)
-
+        
     recursive_file_copy(static_dir, public_dir)
-    generate_pages_recursive("content", "./template.html", "public")
+    generate_pages_recursive(content_dir, template_path, public_dir, basepath)
+    
+
+    # if os.path.exists(public_dir):
+    #     shutil.rmtree(public_dir)
+
+    # recursive_file_copy(static_dir, public_dir)
+    # generate_pages_recursive("content", "./template.html", "public")
 
 main()
